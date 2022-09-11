5438a27e0d6999fde7d67419b092f78d038dc9b6
pritom5928 <paul15-5928@diu.edu.bd>
2022-09-11 22:03:26 +0600

    180. Consecutive Numbers solved in naive approach by correlated subquery

---
+++ 180. Consecutive Numbers/180. Consecutive Numbers.sql
@@ -0,0 +1,56 @@
+180. Consecutive Numbers
+Medium
+
+
+SQL Schema
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+
+
+Naive Solution: 
+
+SELECT 
+	DISTINCT(l1.Num) AS ConsecutiveNums
+FROM Logs l1, Logs l2, Logs l3 
+WHERE (l2.id = l1.id+1 AND l3.id = l2.id+1) AND (l1.num = l2.num AND l1.num = l3.num);
