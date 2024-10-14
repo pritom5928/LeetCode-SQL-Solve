f0790b285f85cde167e8f427a70f0b8702cfeef5
pritom5928 <pritompal.softifybd@gmail.com>
2024-10-14 12:04:41 +0600

    1251. Average Selling Price -  Solution by Join and Window function in MySQL Runtime 685 ms Beats 62.63%

--- 1251. Average Selling Price/1251. Average Selling Price.sql
+++ 1251. Average Selling Price/1251. Average Selling Price.sql
@@ -84,3 +84,14 @@ FROM  prices p LEFT JOIN unitssold u
     ON p.product_id = u.product_id 
     AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
+
+2.  Solution by Join and Window function in MySQL Runtime 685 ms Beats 62.63% :
+
+SELECT 
+    DISTINCT(p.product_id),
+    IFNULL(ROUND(SUM(p.price * u.units) OVER w / SUM(u.units) OVER w, 2), 0) AS average_price
+FROM  prices p 
+LEFT JOIN unitssold u 
+    ON p.product_id = u.product_id 
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+WINDOW w AS (PARTITION BY p.product_id);
