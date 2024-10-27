0dd71352bba1f728944cab290ff0e404832c45b5
pritom5928 <pritompal.softifybd@gmail.com>
2024-10-27 19:10:44 +0600

    570. Managers with at Least 5 Direct Reports - Solution with join Runtime 349 ms & beats 80.55% MySQL online submissions

--- 570. Managers with at Least 5 Direct Reports/570. Managers with at Least 5 Direct Reports.sql
+++ 570. Managers with at Least 5 Direct Reports/570. Managers with at Least 5 Direct Reports.sql
@@ -71,3 +71,13 @@ WHERE 5 <= (
         FROM employee e1 
         WHERE e.id = e1.managerid
 );
+
+
+3. Solution with join Runtime 349 ms & beats 80.55% MySQL online submissions:
+
+SELECT 
+    e.name
+FROM employee e 
+JOIN employee e1 ON e.id = e1.managerid
+GROUP BY e.id
+HAVING COUNT(*) >= 5;
