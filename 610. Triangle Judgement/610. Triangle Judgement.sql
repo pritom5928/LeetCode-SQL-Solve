fd8a31d4b2a52c24b0522c2be9c388e12cd2d853
pritom5928 <pritompal.softifybd@gmail.com>
2024-11-03 12:33:47 +0600

    610. Triangle Judgement - Optimal solution with runtime 284ms beats 64.74% MySQL Submission

--- 610. Triangle Judgement/610. Triangle Judgement.sql
+++ 610. Triangle Judgement/610. Triangle Judgement.sql
@@ -43,7 +43,7 @@ Output:
 
 
 
-solution with Runtime 646 ms Beats 10.2% MySQL submission:
+1. solution with Runtime 646 ms Beats 10.2% MySQL submission:
 
 select x,y,z,
     CASE
@@ -51,3 +51,18 @@ select x,y,z,
         else 'No'
     end as triangle
 from Triangle
+
+2. Optimal solution with runtime 284ms beats 64.74% MySQL Submission:
+
+SELECT 
+    t.*,
+    CASE 
+        WHEN (t.x + t.y <= t.z AND t.x + t.y > 0) 
+             OR (t.x + t.z <= t.y AND t.x + t.z > 0) 
+             OR (t.y + t.z <= t.x AND t.y + t.z > 0)
+            THEN "No"
+        WHEN t.x < 0 OR t.y < 0 OR t.z < 0
+            THEN "No"
+        ELSE "Yes"
+    END AS triangle
+FROM  Triangle t;
