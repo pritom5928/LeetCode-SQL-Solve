ab3ed7e5fbdb3302b52dcff798e010d7a117692b
pritom5928 <pritompal.softifybd@gmail.com>
2024-11-13 16:33:01 +0600

    626. Exchange Seats - Solution by WINDOW FUNCTION with Runtime 334ms Beats 90.20% of MySQL online submissions

--- 626. Exchange Seats/626. Exchange Seats.sql
+++ 626. Exchange Seats/626. Exchange Seats.sql
@@ -68,7 +68,7 @@ FROM Seat
 ORDER BY Id;
 
 
-2. Optimal solution for large Datasets by LEFT JOIN with Runtime 325ms Beats 94.025 of MySQL online submissions :
+2. Optimal solution for large Datasets by LEFT JOIN with Runtime 325ms Beats 94.02% of MySQL online submissions :
 
 SELECT 
     s.id,
@@ -77,3 +77,23 @@ FROM Seat s
 LEFT JOIN  Seat c 
 	ON (s.id % 2 != 0 AND s.id + 1 = c.id) 
     OR (s.id % 2 = 0 AND s.id - 1 = c.id);
+	
+
+3.Solution by WINDOW FUNCTION with Runtime 334ms Beats 90.20% of MySQL online submissions:
+
+SELECT 
+    CASE 
+        WHEN Id < MAX(Id) OVER () THEN 
+            CASE 
+                WHEN Id % 2 = 1 THEN LEAD(Id) OVER (ORDER BY Id)  
+                ELSE LAG(Id) OVER (ORDER BY Id)                  
+            END
+        ELSE 
+            CASE 
+                WHEN Id % 2 = 1 THEN Id                         
+                ELSE LAG(Id) OVER (ORDER BY Id)                 
+            END
+    END AS id,
+    student
+FROM Seat
+ORDER BY Id;
