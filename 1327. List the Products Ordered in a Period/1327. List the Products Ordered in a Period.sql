ac4f9c8044a08ef75983a4499499b621f4bc2cf2
pritom5928 <pritompal.softifybd@gmail.com>
2024-12-10 17:30:44 +0600

    1327. List the Products Ordered in a Period - Time & Space complexity added to solution with Join & GROUP BY with Runtime 762 ms (1)

--- 1327. List the Products Ordered in a Period/1327. List the Products Ordered in a Period.sql
+++ 1327. List the Products Ordered in a Period/1327. List the Products Ordered in a Period.sql
@@ -91,3 +91,6 @@ FROM products p JOIN Orders o ON p.product_id = o.product_id
 WHERE o.order_date >= '2020-02-01' AND o.order_date <= '2020-02-29'
 GROUP BY p.product_id
 HAVING unit >= 100;
+
+	- Time complexity: O(N * M), here N = numbers of products, M = number of orders
+	- Space complexity: O(N + M + G), G = numbers of total unique products on orders table after satisfying WHERE condition 
