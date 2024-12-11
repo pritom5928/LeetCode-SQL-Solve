0a0e680727858ed77715cbdf4f49ced80b814df7
pritom5928 <pritompal.softifybd@gmail.com>
2024-12-11 16:53:09 +0600

    180. Consecutive Numbers - Solution with Self Join Runtime 579ms (Beats 64.83%)

--- 180. Consecutive Numbers/180. Consecutive Numbers.sql
+++ 180. Consecutive Numbers/180. Consecutive Numbers.sql
@@ -57,3 +57,13 @@ SELECT
 	DISTINCT(l1.Num) AS ConsecutiveNums
 FROM Logs l1, Logs l2, Logs l3 
 WHERE (l2.id = l1.id+1 AND l3.id = l2.id+1) AND (l1.num = l2.num AND l1.num = l3.num);
+
+
+2. Solution with Self Join Runtime 579ms (Beats 64.83%):
+
+SELECT 
+	DISTINCT l1.Num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l2.id = l1.id + 1
+JOIN Logs l3 ON l3.id = l2.id + 1
+WHERE l1.num = l2.num AND l1.num = l3.num;
