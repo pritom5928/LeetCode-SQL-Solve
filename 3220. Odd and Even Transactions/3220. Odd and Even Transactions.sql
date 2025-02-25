28f9dff26dc4db3fcb6b19141303cdedc37a1f05
pritom5928 <pritompal.softifybd@gmail.com>
2025-02-25 18:49:34 +0600

    3220. Odd and Even Transactions - Solution with CTE & Window function with Runtime 300ms beats 91.95% (2)

--- 3220. Odd and Even Transactions/3220. Odd and Even Transactions.sql
+++ 3220. Odd and Even Transactions/3220. Odd and Even Transactions.sql
@@ -74,3 +74,24 @@ SELECT
 FROM transactions
 GROUP BY transaction_date
 ORDER BY transaction_date;
+
+2. Solution with CTE & Window function with Runtime 300ms beats 91.95%:
+
+  - Time Complexity: O(N log G) => G is the numbers of unique transactions date
+  - Space Complexity: O(N)
+
+WITH cte AS (
+    SELECT 
+        t.transaction_date,
+        SUM(IF(amount % 2 = 1, amount, 0)) OVER (PARTITION BY transaction_date) AS odd_sum,
+        SUM(IF(amount % 2 = 0, amount, 0)) OVER (PARTITION BY transaction_date) AS even_sum,
+        ROW_NUMBER() OVER (PARTITION BY transaction_date) AS rn
+    FROM transactions t
+    ORDER BY t.transaction_date
+)
+SELECT 
+    c.transaction_date,
+    c.odd_sum,
+    c.even_sum
+FROM cte c
+WHERE c.rn = 1;
